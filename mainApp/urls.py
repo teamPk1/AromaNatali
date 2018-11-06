339334ce80e0d75538f993536262484a998099fe
varvadyk <39148487+varvadyk@users.noreply.github.com>
2018-11-06 11:43:27 +0200

    Update urls.py

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -7,9 +7,7 @@ urlpatterns = [
     path('product/<int:product_id>', views.product, name='product'),
     path('menu', views.menu, name='menu'),
     path('about', views.about, name="about"),
-<<<<<<< HEAD
     path('registration',views.registration,name="registration")
-=======
     path("128asd122819u", views.sendEmail, name="sendEmail")
->>>>>>> e248d1485cee3c2e4eec00de79301fb2751c871e
+
     ]
