7fb3c62aa17d6c2abc3b738b9f1b19fcf5b2fbbb
vadimuha <vadimuha13@gmail.com>
2018-11-18 20:34:05 +0200

    small fixes

--- mainApp/models.py
+++ mainApp/models.py
@@ -10,24 +10,13 @@ class Product(models.Model):
 	description = models.TextField()
 	gender = models.IntegerField(default = -1) # 0 men 1 woman 2 both
 	price = models.DecimalField(max_digits = 6, decimal_places = 2)
-	amount_in_basket = models.IntegerField(default = 0) 
 	is_featured = models.IntegerField(default = 0)
 	is_transit = models.IntegerField(default = 1) # is in procces of creation 1 yes 0 no
 
 	def __str__(self):
 		return self.name
 
-class Order(models.Model):
-	product_name = models.ForeignKey(Product, default = None, on_delete = models.CASCADE)
-	# Deletion of product should be possible only when there's no product in waiting
-	user_name = models.ForeignKey(User, default = None, on_delete = models.CASCADE)
-	status = models.IntegerField(default = 0) # 0 waiting, 1 sent 2 arrived 3 collected
-	
-class Review(models.Model):
-	product_name = models.ForeignKey(Product, default = None, on_delete = models.CASCADE)
-	rate = models.IntegerField()
-	review = models.TextField()
-	user_name = models.ForeignKey(User, default = None, on_delete = models.CASCADE)
+
 class Profile (models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	phone_number = models.CharField(max_length=30) 
